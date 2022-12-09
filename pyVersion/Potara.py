d631a0b985c60a7a03d00fd83434b172c75da373
FaresAmiar <45738287+FaresAmiar@users.noreply.github.com>
2022-12-09 17:35:08 +0100

    Update Potara.py

--- pyVersion/Potara.py
+++ pyVersion/Potara.py
@@ -1,7 +1,7 @@
 class Potara:
 	def __init__(self,Saiyan s = None):
-		self.s = s
+		self.__s = s
 	
 	def improveKamehameha(self,Saiyan s2):
-		s.setKi((int)(s.getKi()*1.1));
-		s.kamehameha(s2)
+		self.__s.setKi((int)(self.__s.getKi()*1.1));
+		self.__s.kamehameha(s2)
