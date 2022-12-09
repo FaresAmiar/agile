41649fedb732f2e6a1d8220e357ce6add7e83b52
FaresAmiar <45738287+FaresAmiar@users.noreply.github.com>
2022-12-09 17:44:56 +0100

    Potara error type confuse java

--- pyVersion/Potara.py
+++ pyVersion/Potara.py
@@ -1,7 +1,9 @@
+import Saiyan
+
 class Potara:
-	def __init__(self,Saiyan s = None):
-		self.__s = s
+	def __init__(self, saiyan = None):
+		self.__s = saiyan
 	
-	def improveKamehameha(self,Saiyan s2):
+	def improveKamehameha(self,s2):
 		self.__s.setKi(self.__s.getKi()*1.1);
 		self.__s.kamehameha(s2)
--- pyVersion/TestPotara.py
+++ pyVersion/TestPotara.py
@@ -1,19 +1,22 @@
 import pytest
 import math
+import Saiyan
+import Potara
 		
 class Test:
 	def __init__(self):
 		self.Goku = Saiyan()
 		self.Vegeta = Saiyan()
-		self.potara = Potara(Vegeta)
+		self.potara = Potara(self.Vegeta)
 	
 	def kamehamehaTest(self):
-		gokuKi = Goku.getKi()
-		vegetaHealth = Vegeta.getHealth()
+		gokuKi = self.Goku.getKi()
+		vegetaHealth = self.Vegeta.getHealth()
 		assert self.Goku.getKi() == gokuKi - self.Goku.KAMEHAMEHA_ATTACK
+		
 	def saiyanPotaraTest(self):
-		vegetaKi = Vegeta.getKi()
-		gokuHealth = Goku.getHealth()
-        self.potara.improveKamehameha(Goku)
-        assert Vegeta.getKi() == math.floor((vegetaKi*1.1) - Goku.KAMEHAMEHA_ATTACK)
-        assert Goku.getHealth() == gokuHealth- Vegeta.KAMEHAMEHA_ATTACK
+		vegetaKi = self.Vegeta.getKi()
+		gokuHealth = self.Goku.getHealth()
+        self.potara.improveKamehameha(self.Goku)
+        assert self.Vegeta.getKi() == math.floor((vegetaKi*1.1) - Goku.KAMEHAMEHA_ATTACK)
+        assert self.Goku.getHealth() == gokuHealth- Vegeta.KAMEHAMEHA_ATTACK
