58af2b8466a81e5138e34eace0a537f6c5469f4c
FaresAmiar <45738287+FaresAmiar@users.noreply.github.com>
2022-12-09 17:33:18 +0100

    Update and rename saiyan.py to Saiyan.py

---
+++ pyVersion/Saiyan.py
@@ -0,0 +1,22 @@
+class Saiyan:
+    def __init__(self, ki = 100, health = 100, KAMEHAMEHA_ATTACK = 30):
+        self.__ki=ki
+        self.__health=health
+        self.KAMEHAMEHA_ATTACK=30
+        
+    def kamehameha(self,s2):
+        if(s2 is not  None and s2.health > 0):
+            self.__ki -= self.KAMEHAMEHA_ATTACK
+            s2.__health -= self.KAMEHAMEHA_ATTACK
+
+    def getKi(self):
+        return self.__ki
+    def getHealth(self):
+        return self.__health
+    def setKi(self,ki):
+        self.__ki=ki
+    def setHealth(self,health):
+        self.__health=health
+    '''def setKAMEHAMEHA_ATTACK(self,KAMEHAMEHA_ATTACK):
+        self.KAMEHAMEHA_ATTACK=KAMEHAMEHA_ATTACK'''
+    
