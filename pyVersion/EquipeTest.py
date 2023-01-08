cca0436d7678c97dc417ac2de1d8ee8322d5a16a
kylan-d <73824522+kylan-d@users.noreply.github.com>
2023-01-08 15:21:41 +0100

    Add files via upload

--- pyVersion/EquipeTest.py
+++ pyVersion/EquipeTest.py
@@ -1,4 +1,5 @@
 import unittest
+import Saiyan
 import Equipe as E
 
 nom_equipe = "Maroc"
@@ -8,12 +9,14 @@ nom_coach = "Walid Regragui"
 class EquipeTestCase(unittest.TestCase):
 
     def test_toString(self):
-        equipe = E.Equipe(nom_equipe, nom_coach)
+        self.Vegeta = Saiyan.Saiyan()
+        equipe = E.Equipe(nom_equipe, nom_coach,[self.Vegeta])
         self.assertEqual("Le nom de votre équipe est " + nom_equipe + " et le nom de votre coach est " + nom_coach,
                          equipe.toString())
 
     def test_get_nom_equipe(self):
-        equipe = E.Equipe(nom_equipe, nom_coach)
+        self.Vegeta = Saiyan.Saiyan()
+        equipe = E.Equipe(nom_equipe, nom_coach,[self.Vegeta])
         self.assertEqual(nom_equipe, equipe.get_nom_equipe())
 
 
