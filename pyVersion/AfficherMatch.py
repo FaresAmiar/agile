f69443127ee0002faf6c3e9ce6478dd51e26f26b
FaresAmiar <45738287+FaresAmiar@users.noreply.github.com>
2023-01-11 16:02:56 +0100

    Ajout main afficherMatch

--- pyVersion/AfficherMatch.py
+++ pyVersion/AfficherMatch.py
@@ -8,16 +8,22 @@ class AfficherMatch(Observateur):
     def update(self, gagnant: Equipe, perdant: Equipe):
         print(f"Le gagnant est {gagnant.get_nom_equipe()}")
 
-factory = EquipeFactory()
 
-# Création des équipes
-equipe1 = factory.creer_equipe("Equipe 1", [(1000, 1000, "Goku"), (900, 900, "Vegeta")], "Whis")
-equipe2 = factory.creer_equipe("Equipe 2", [(800, 800, "Gohan"), (700, 700, "Trunks")], "Piccolo")
 
-# Création du match et ajout de l'observateur
-match = Match(equipe1, equipe2)
-affichage = AfficherMatch()
-match.ajouter_observateur(affichage)
+def main():
+    factory = EquipeFactory()
 
-# Affrontement des équipes
-match.affronter()
+    # Création des équipes
+    equipe1 = factory.creer_equipe("Gardiens de la Galaxie", [(1000, 1000, "Goku"), (900, 900, "Vegeta")], "Whis")
+    equipe2 = factory.creer_equipe("Destructeurs de Monde", [(800, 800, "Gohan"), (700, 700, "Trunks")], "Piccolo")
+
+    # Création du match et ajout de l'observateur
+    match = Match(equipe1, equipe2)
+    affichage = AfficherMatch()
+    match.ajouter_observateur(affichage)
+
+    # Affrontement des équipes
+    match.affronter()
+    
+if __name__ == '__main__':
+    main()
